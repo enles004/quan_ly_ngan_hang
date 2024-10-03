1aa14170705a3797c5b47dd8f6f7d0073d1277fe
enles004 <133115870+enles004@users.noreply.github.com>
2024-10-03 16:48:28 +0700

    mot chut

---
+++ src/main/java/com/mycompany/models/user.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.models;
+
+/**
+ *
+ * @author nobpe
+ */
+public class user {
+    String id;
+    String ho;
+    String ten;
+    String ngay_sinh;
+    String gioi_tinh;
+    String dia_chi;
+    String sdt;
+    String email;
+    String cccd;
+    String stk;
+    
+    public user(){
+    }
+    
+    
+}
