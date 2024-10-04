6ab4e28fb1dcc90158e8bfd4c93ad395b39fb7da
enles004 <133115870+enles004@users.noreply.github.com>
2024-10-04 14:32:51 +0700

    ok

--- src/main/java/com/mycompany/controllers/admin/nguoi_dung.java
+++ src/main/java/com/mycompany/controllers/admin/nguoi_dung.java
@@ -5,17 +5,23 @@
 package com.mycompany.controllers.admin;
 
 import com.mycompany.db;
+import com.mycompany.models.user;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import javax.swing.plaf.basic.BasicInternalFrameUI;
 import javax.swing.table.DefaultTableModel;
 
@@ -56,6 +62,10 @@ public class nguoi_dung extends javax.swing.JInternalFrame {
             Image scaledImg_thoat = img_thoat.getScaledInstance(20, 20, Image.SCALE_SMOOTH);
             btn_thoat.setIcon(new ImageIcon(scaledImg_thoat));
             
+            BufferedImage img_tk = ImageIO.read(new File("src/main/java/com/mycompany/pics/search.png"));
+            Image scaledImg_tk = img_tk.getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+            tk.setIcon(new ImageIcon(scaledImg_tk));
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -74,6 +84,8 @@ public class nguoi_dung extends javax.swing.JInternalFrame {
         jPanel6 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         sum_nd = new javax.swing.JLabel();
+        txt_tk = new javax.swing.JTextField();
+        tk = new javax.swing.JLabel();
         jPanel7 = new javax.swing.JPanel();
         btn_sua = new javax.swing.JButton();
         btn_xoa = new javax.swing.JButton();
@@ -142,7 +154,7 @@ public class nguoi_dung extends javax.swing.JInternalFrame {
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 257, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 286, Short.MAX_VALUE)
                 .addGap(0, 0, 0))
         );
 
@@ -175,6 +187,18 @@ public class nguoi_dung extends javax.swing.JInternalFrame {
         sum_nd.setForeground(new java.awt.Color(204, 0, 51));
         sum_nd.setText("jLabel3");
 
+        txt_tk.setToolTipText("");
+        txt_tk.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                txt_tkMouseClicked(evt);
+            }
+        });
+        txt_tk.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txt_tkKeyReleased(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
         jPanel6.setLayout(jPanel6Layout);
         jPanel6Layout.setHorizontalGroup(
@@ -184,23 +208,42 @@ public class nguoi_dung extends javax.swing.JInternalFrame {
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(sum_nd, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(93, 93, 93)
+                .addComponent(txt_tk, javax.swing.GroupLayout.PREFERRED_SIZE, 282, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(tk, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         jPanel6Layout.setVerticalGroup(
             jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel6Layout.createSequentialGroup()
-                .addGap(0, 0, 0)
-                .addGroup(jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(sum_nd))
+                .addGroup(jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(txt_tk, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(tk, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel6Layout.createSequentialGroup()
+                            .addGroup(jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(jLabel2)
+                                .addComponent(sum_nd))
+                            .addGap(1, 1, 1))))
                 .addContainerGap())
         );
 
         jPanel7.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
 
         btn_sua.setText("Sửa");
+        btn_sua.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_suaActionPerformed(evt);
+            }
+        });
 
         btn_xoa.setText("Xóa");
+        btn_xoa.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_xoaActionPerformed(evt);
+            }
+        });
 
         btn_thoat.setText("Thoát");
         btn_thoat.addActionListener(new java.awt.event.ActionListener() {
@@ -234,7 +277,7 @@ public class nguoi_dung extends javax.swing.JInternalFrame {
 
         txt_ns.setDateFormatString("yyyy/MM/dd");
 
-        txt_gt.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "-Chọn giới tính-", "Nam", "Nữ", "Khác", " " }));
+        txt_gt.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Nam", "Nữ", "Khác", " " }));
 
         javax.swing.GroupLayout jPanel8Layout = new javax.swing.GroupLayout(jPanel8);
         jPanel8.setLayout(jPanel8Layout);
@@ -255,9 +298,7 @@ public class nguoi_dung extends javax.swing.JInternalFrame {
                     .addComponent(txt_ns, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
                 .addGroup(jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel8Layout.createSequentialGroup()
-                        .addComponent(jLabel8)
-                        .addGap(96, 96, 96))
+                    .addComponent(jLabel8)
                     .addGroup(jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                         .addComponent(txt_sdt)
                         .addComponent(jLabel7, javax.swing.GroupLayout.Alignment.LEADING)
@@ -369,11 +410,11 @@ public class nguoi_dung extends javax.swing.JInternalFrame {
         System.exit(0);
         // TODO add your handling code here:
     }//GEN-LAST:event_btn_thoatActionPerformed
-
+    user u;
     private void tb_ndMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tb_ndMouseClicked
         int i = tb_nd.getSelectedRow();
-        btn_sua.setEnabled(true);
-        btn_xoa.setEnabled(true);
+        btn_sua.setVisible(true);
+        btn_xoa.setVisible(true);
         txt_ho.setEnabled(true);
         txt_ten.setEnabled(true);
         txt_ns.setEnabled(true);
@@ -395,11 +436,103 @@ public class nguoi_dung extends javax.swing.JInternalFrame {
         txt_dc.setText(tb.getValueAt(i, 4).toString());
         txt_sdt.setText(tb.getValueAt(i, 5).toString());
         txt_cccd.setText(tb.getValueAt(i, 7).toString());
-        // TODO add your handling code here:
+        u = new user();
+        u.setStk(tb.getValueAt(i, 8).toString());
     }//GEN-LAST:event_tb_ndMouseClicked
-    Connection con;
-    
-    private void load(){
+
+    private void btn_suaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_suaActionPerformed
+        try {
+            con = db.connect();
+            String ho = txt_ho.getText().trim();
+            String ten = txt_ten.getText().trim();
+            Date ns = new Date(txt_ns.getDate().getTime());
+            String gt = txt_gt.getSelectedItem().toString();
+            String dt = txt_sdt.getText().trim();
+            String dc = txt_dc.getText().trim();
+            String cd = txt_cccd.getText().trim();
+            String sql = "Update thong_tin_ca_nhan Set ho = N'"+ho+"', ten = N'"+ten+"', ngay_sinh = '"+ns+"', gioi_tinh = N'"+gt+"', dia_chi = N'"+dc+"', so_dien_thoai = '"+dt+"', so_cong_dan = '"+cd+"'"
+                    + "where so_tai_khoan = '"+u.getStk()+"'";
+            Statement st = con.createStatement();
+            st.executeUpdate(sql);
+            JOptionPane.showMessageDialog(this, "Bạn đã cập nhật thành công");
+            con.close();
+            load();
+        } catch (SQLException ex) {
+            Logger.getLogger(nguoi_dung.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(nguoi_dung.class.getName()).log(Level.SEVERE, null, ex);
+        }        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_btn_suaActionPerformed
+
+    private void btn_xoaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_xoaActionPerformed
+        try {
+            con = db.connect();
+            Statement st = con.createStatement();
+            String sql = "Delete nd from nguoi_dung nd "
+                    + "join thong_tin_ca_nhan ttcn on nd.id = ttcn.nguoi_dung_id "
+                    + "where ttcn.so_tai_khoan = '"+u.getStk()+"'";
+            int r = JOptionPane.showConfirmDialog(this, "Bạn có muốn xóa người dùng này không?", "Xác nhận", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            if(r == JOptionPane.YES_OPTION){
+                st.executeUpdate(sql);
+                JOptionPane.showMessageDialog(this, "Xóa thành công!!");
+                con.close();
+            }
+            load();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn_xoaActionPerformed
+
+    private void txt_tkKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txt_tkKeyReleased
+        try {
+            con = db.connect();
+            Statement st = con.createStatement();
+            String tim = txt_tk.getText().trim();
+            String sql = "select * "
+                    + "from thong_tin_ca_nhan ttcn "
+                    + "join nguoi_dung nd on nd.id = ttcn.nguoi_dung_id "
+                    + "where ttcn.so_dien_thoai like '"+tim+"%'"; 
+            ResultSet rs = st.executeQuery(sql);
+            String[] td = {"Họ", "Tên", "Ngày sinh", "Giới tính", "Địa chỉ", "Số điện thoại", "Email", "Số công dân", "Số tài khoản"};
+            DefaultTableModel tb = new DefaultTableModel(td, 0){
+                @Override
+                public boolean isCellEditable(int row, int column) {
+                    return false; // Không cho phép chỉnh sửa bất kỳ ô nào
+                }
+            };
+            System.out.println(rs);
+            while(rs.next()){
+                Vector v = new Vector();
+                v.add(rs.getString("ho"));
+                v.add(rs.getString("ten"));
+                v.add(rs.getString("ngay_sinh"));
+                v.add(rs.getString("gioi_tinh"));
+                v.add(rs.getString("dia_chi"));
+                v.add(rs.getString("so_dien_thoai"));
+                v.add(rs.getString("email"));
+                v.add(rs.getString("so_cong_dan"));
+                v.add(rs.getString("so_tai_khoan"));
+                tb.addRow(v);
+            }
+            tb_nd.setModel(tb);
+            con.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txt_tkKeyReleased
+
+    private void txt_tkMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txt_tkMouseClicked
+        txt_ho.setText("");
+        txt_ten.setText("");
+        txt_ns.setDate(null);
+        txt_gt.setSelectedItem(" ");
+        txt_dc.setText("");
+        txt_sdt.setText("");
+        txt_cccd.setText("");
         txt_ho.setEnabled(false);
         txt_ten.setEnabled(false);
         txt_ns.setEnabled(false);
@@ -407,8 +540,29 @@ public class nguoi_dung extends javax.swing.JInternalFrame {
         txt_dc.setEnabled(false);
         txt_sdt.setEnabled(false);
         txt_cccd.setEnabled(false);
-        btn_sua.setEnabled(false);
-        btn_xoa.setEnabled(false);
+        btn_sua.setVisible(false);
+        btn_xoa.setVisible(false);
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txt_tkMouseClicked
+    Connection con;
+    
+    private void load(){
+        txt_ho.setText("");
+        txt_ten.setText("");
+        txt_ns.setDate(null);
+        txt_gt.setSelectedItem(" ");
+        txt_dc.setText("");
+        txt_sdt.setText("");
+        txt_cccd.setText("");
+        txt_ho.setEnabled(false);
+        txt_ten.setEnabled(false);
+        txt_ns.setEnabled(false);
+        txt_gt.setEnabled(false);
+        txt_dc.setEnabled(false);
+        txt_sdt.setEnabled(false);
+        txt_cccd.setEnabled(false);
+        btn_sua.setVisible(false);
+        btn_xoa.setVisible(false);
         try {
             tb_nd.removeAll();
             con = db.connect();
@@ -470,6 +624,7 @@ public class nguoi_dung extends javax.swing.JInternalFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel sum_nd;
     private javax.swing.JTable tb_nd;
+    private javax.swing.JLabel tk;
     private javax.swing.JTextField txt_cccd;
     private javax.swing.JTextField txt_dc;
     private javax.swing.JComboBox<String> txt_gt;
@@ -477,5 +632,6 @@ public class nguoi_dung extends javax.swing.JInternalFrame {
     private com.toedter.calendar.JDateChooser txt_ns;
     private javax.swing.JTextField txt_sdt;
     private javax.swing.JTextField txt_ten;
+    private javax.swing.JTextField txt_tk;
     // End of variables declaration//GEN-END:variables
 }
