a8ef158077a8c7fc3b6e5a62aee8845d0bd0f8f2
HOA.HP <HOA.HP@NGUYENHOA>
2024-10-11 15:13:05 +0700

    ok

--- src/main/java/com/mycompany/controllers/user/Quanlithongtincanhan.java
+++ src/main/java/com/mycompany/controllers/user/Quanlithongtincanhan.java
@@ -629,6 +629,7 @@ public class Quanlithongtincanhan extends javax.swing.JInternalFrame {
                 spreadsheet.autoSizeColumn(col);
             }
             File f = new File("D:\\Java nhóm 9\\quan_ly_ngan_hang\\Danh_sach_thong_tin_ca_nhan.xlsx");
+            //File f = new File("D:\\Danhsachtacgia.xlsx");
             FileOutputStream out = new FileOutputStream(f);
             workbook.write(out);
             out.close();
@@ -706,8 +707,12 @@ public class Quanlithongtincanhan extends javax.swing.JInternalFrame {
         //ket noi database
         Connection con = null;
         try {
+            //            Class.forName("com.mysql.cj.jdbc.Driver");
+            //            con=DriverManager.getConnection(url,user,pass);
+
             con = db.connect();
-           
+            //            String sql="Insert Tacgia(Matacgia, Tentacgia,NgaySinh, Gioitinh,Dienthoai, Email, Diachi) Values('"+mtg+"',N'"+ttg+"','"+ngs+"',N'"+gt+"',"+"'"+dt+"','"+email+"',N'"+dc+"')";
+            //            Statement st=con.createStatement();
             String sql="Insert into thong_tin_ca_nhan Values(?,?,?,?,?,?,?,?,?)";
             PreparedStatement st=con.prepareStatement(sql);
             st.setString(0,id);
@@ -765,7 +770,8 @@ public class Quanlithongtincanhan extends javax.swing.JInternalFrame {
     private void load_thongtin() throws ClassNotFoundException{
     Connection con = null;
         try {
-
+////            Class.forName("com.mysql.cj.jdbc.Driver");
+////            con=DriverManager.getConnection(url,user,pass);
             con = db.connect();
             String sql="Select * from thong_tin_ca_nhan";
             Statement st=con.createStatement();
@@ -843,15 +849,11 @@ public class Quanlithongtincanhan extends javax.swing.JInternalFrame {
     private javax.swing.JPanel jPanel6;
     private javax.swing.JPanel jPanel7;
     private javax.swing.JScrollPane jScrollPane1;
-<<<<<<< HEAD
-    private javax.swing.JTable tbthongtin;
-=======
     private javax.swing.JLabel nguoi_dung;
     private javax.swing.JTable tbthongtin;
     private javax.swing.JLabel the_canhan;
     private javax.swing.JLabel tiet_kiem;
     private javax.swing.JLabel tra_no;
->>>>>>> 614f993fc410b534801a53954c76f80517bda379
     private javax.swing.JTextField txtDiachi;
     private javax.swing.JTextField txtHo;
     private javax.swing.JTextField txtID;
